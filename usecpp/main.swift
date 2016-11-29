2a7afd65fd71fe1ca3668787f2fda1cb60c2f7cc
Richard Hodges <hodges.r@gmail.com>
2016-11-29 13:09:52 +0100

    Initial Commit

---
+++ usecpp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  usecpp
+//
+//  Created by Richard Hodges on 29/11/2016.
+//  Copyright © 2016 Richard Hodges. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
